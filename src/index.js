c53e091b6c544dfd0eea26af7184eb2264dcb205
Kirsten Hunter <synedra@mac.lan>
2025-10-21 08:47:22 -0700

    Removing extra auth callback

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App.js';
-import AuthCallback from './AuthCallback.js';
 import { AppProvider } from './AppContext.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,7 +12,6 @@ root.render(
       <Router>
         <Routes>
           <Route path="/" element={<App />} />
-          <Route path="/auth/callback" element={<AuthCallback />} />
         </Routes>
       </Router>
     </AppProvider>
